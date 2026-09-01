9fc04a553274ad2e25d375f1abb57452f671efc9
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-01 11:24:34 +0530

    Add array reversal methods in 02_reverse_array.py
    Implemented two methods to reverse an array: a brute force approach using two pointers and a slicing method.

---
+++ Array/02_reverse_array.py
@@ -0,0 +1,15 @@
+#BRUTE FORCE (2 pointer)
+def reverse(nums,left,right):
+    while(left<right):
+        nums[left],nums[right]=nums[right],nums[left]
+        left=left+1
+        right=right-1
+nums=[1,2,3,4,5,6,7,8,9,10]
+n=len(nums)
+reverse(nums,0,n-1)
+print(nums)   
+
+#USING SLICING
+nums=[1,2,3,4,5,6,7,8,9,10]
+nums=nums[::-1]
+print(nums)
